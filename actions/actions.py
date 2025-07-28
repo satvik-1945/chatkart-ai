5d1bee4c59fdb84dab3ee310b159fbeeed0d6ac6
satvik-1945 <satviktejas@gmail.com>
2025-07-28 22:08:34 +0530

    new model

--- actions/actions.py
+++ actions/actions.py
@@ -1,16 +1,8 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
 from typing import Any, Text, Dict, List, Optional
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher  
 from rasa_sdk.events import SlotSet
 
-# in memory database for now 
 USER_PROFILE = {}
 ORDERS = {}
 
